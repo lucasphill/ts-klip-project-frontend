7ff92138cefacb8cfe23f6125fd113adbe45ad2f
lucasphill <lucaspscp@outlook.com>
2026-03-22 21:18:37 -0300

    Add analytics

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import './index.css'
 import App from './App.tsx'
 import { LoadingProvider } from './contexts/LoadingContext.tsx'
 import { Auth0Provider } from '@auth0/auth0-react'
-import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -22,6 +21,5 @@ createRoot(document.getElementById('root')!).render(
         <App />
       </Auth0Provider>
     </LoadingProvider>
-    <Analytics />
   </StrictMode>
 )
